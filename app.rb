6202215ca16cf878cc8212d225f660e763a2e065
Katie Ediger <katieediger@gmail.com>
2018-01-17 17:46:54 -0600

    add initial files to git

---
+++ app.rb
@@ -0,0 +1,3 @@
+pet_name = 'Hank'
+
+puts "Hey! #{pet_name.upcase} is my pet's name"
---
+++ car.rb
@@ -0,0 +1,6 @@
+class Car
+  def initialize(make)
+    @make = make
+    puts "You made a new car! A brand-neew #{@make}!"
+  end
+end
